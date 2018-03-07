9dd5bf3c7af0db49becd62ece62fc3e43cc1599b
oscar.manzo <oscar.manzo@globant.com>
2018-03-07 01:35:57 +0000

    mas ejercicios

---
+++ src/com/globant/google/nest/sort/Solution.java
@@ -0,0 +1,74 @@
+package com.globant.google.nest.sort;
+
+import java.util.*;
+
+class Student implements Comparable<Student> {
+   private int id;
+   private String fname;
+   private double cgpa;
+
+   public Student(int id, String fname, double cgpa) {
+      super();
+      this.id = id;
+      this.fname = fname;
+      this.cgpa = cgpa;
+   }
+   public int getId() {
+      return id;
+   }
+   public String getFname() {
+      return fname;
+   }
+   public double getCgpa() {
+      return cgpa;
+   }
+    
+   public int compareTo(Student o){
+        int result = 0;
+        
+        if (this.cgpa < o.cgpa) result = 1;
+        if (this.cgpa == o.cgpa) result = 0;
+        if (this.cgpa > o.cgpa) result = -1;
+        
+        if (result==0){
+            result = this.fname.compareTo(o.fname);
+        }
+        
+        if (result==0){
+            if (this.id < o.id) result = -1;
+            if (this.id == o.id) result = 0;
+            if (this.id > o.id) result = 1;
+        }
+       
+        return result;
+   }
+}
+
+//Complete the code
+public class Solution {
+
+   public static void main(String[] args){
+      Scanner in = new Scanner(System.in);
+      int testCases = Integer.parseInt(in.nextLine());
+      
+      List<Student> studentList = new ArrayList<Student>();
+      while(testCases>0){
+         int id = in.nextInt();
+         String fname = in.next();
+         double cgpa = in.nextDouble();
+         
+         Student st = new Student(id, fname, cgpa);
+         studentList.add(st);
+         
+         testCases--;
+      }
+      
+        Collections.sort(studentList);
+       
+      for(Student st: studentList){
+         System.out.println(st.getFname());
+      }
+
+   }
+
+}
